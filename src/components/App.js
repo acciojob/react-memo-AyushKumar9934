082dc5340f1ee531bf1e787514bfb539caa96a49
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-13 20:01:12 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,59 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useMemo, useState } from 'react'
+import SkillComp from './SkillComp';
+
+const App = () => {
+   const [todos,setTodos]=useState([]);
+   const [skill,setSkill]=useState("")
+   const [allSkill,setAllSkill]=useState([]);
+   function handleAddtodo(){
+    setTodos([...todos,[]]);
+   }
+   const expensive=useMemo(()=>{
+    let res=0;
+    for(let i=0;i<=1000000000;i++){
+        res+=i;
+    }
+   return res},[]);
+   function AddSkill(){
+    console.log("we are inside add skill")
+    console.log(skill);
+   setAllSkill([...allSkill,skill]);
+  
+   }
+   console.log(allSkill)
+   
+  
+  return (
+    <div>
+    <h1>React.useMemo</h1>
+    <h1>My todos</h1>
+    {
+        todos.map((item,i)=>{
+           return <p>New Todos</p>
+        })
+
+    }
+    <button onClick={handleAddtodo}>Add todo</button>
+    <hr style={{width:"100%"}} ></hr>
+    <h1>Expensive Calculation</h1>
+    {expensive}
+    <hr style={{width:"100%"}}></hr>
+    <hr style={{width:"100%"}}></hr>
+    <h1>React.memo</h1>
+    <input placeholder='Add Skills' value={skill} onChange={(e)=>setSkill(e.target.value)}/>
+
+    <button onClick={AddSkill}>Add Skill</button>
+   <ul>{
+       allSkill.map((item,i)=>{
+    return     <SkillComp key={i} item={item}/>
+       
+        })
+    }</ul> 
+    
+    </div>
+  )
+}
+
+export default App
+
 
---
+++ src/components/SkillComp.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+const SkillComp =memo(({item}) => {
+  return (
+    <li>{item}</li>
+  )
+})
+
+export default SkillComp
